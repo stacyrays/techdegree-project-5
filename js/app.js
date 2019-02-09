0b9d796ed2ccf12c788af66ebd617d7a433f4a7d
Stacy <stacyrays@gmail.com>
2019-02-09 13:35:02 -0800

    Creating and adding first html block from the data

--- js/app.js
+++ js/app.js
@@ -1,9 +1,30 @@
 //cd Desktop/LZ/Tutorials/Treehouse/TechDegree/techdegree-project-5
 //python -m SimpleHTTPServer
+const $gallery = $("#gallery");
 $.ajax({
-  url: "https://randomuser.me/api/",
+  url: "https://randomuser.me/api/?results=12",
   dataType: "json",
   success: function(data) {
     console.log(data);
+    console.log(data.results[0].picture.large);
+    console.log(data.results[0].name.first);
+    $('<div class="card"/>').appendTo($gallery);
+    $('<div class="card-img-container"/>').appendTo(".card");
+    $('<img class="card-img" src="#" alt="profile picture">').appendTo(
+      ".card-img-container"
+    );
+
+    $(".card-img").attr("src", data.results[0].picture.large);
+
+    $('<div class="card-info-container">').insertAfter(".card-img-container");
+
+    $('<h3 id="name" class="card-name cap" />').appendTo(
+      ".card-info-container"
+    );
+    $("h3#name").text(
+      data.results[0].name.first + " " + data.results[0].name.last
+    );
+    $('<p class="card-text">email</p>').insertAfter(".card-name");
+    $('<p class="card-text cap">city, state</p>').insertAfter(".card-text");
   }
 });
