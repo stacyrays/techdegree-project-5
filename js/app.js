260e2c8de49a6b0377bc81bd521d13c60df63a57
Stacy <stacyrays@gmail.com>
2019-02-12 14:52:32 -0800

    One modal pulls in different data now

--- js/app.js
+++ js/app.js
@@ -3,6 +3,38 @@
 let $gallery = $("div#gallery.gallery");
 let $modalContainer = $('<div class="modal-container"/>').insertAfter($gallery);
 
+let $modal = $('<div class="modal"/>').appendTo($modalContainer);
+let $btnClose = $(
+  '<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>'
+).appendTo($modal);
+let $modalInfoContainer = $('<div class="modal-info-container" />').insertAfter(
+  $btnClose
+);
+//Build modal
+$modalImage = $(
+  `<img class="modal-img" src="#" alt="profile picture">`
+).appendTo($modalInfoContainer);
+
+$modalName = $(`<h3 id="name" class="modal-name cap"></h3>`).appendTo(
+  $modalInfoContainer
+);
+
+$modalEmail = $(`<p class="modal-text"></p>`).appendTo($modalInfoContainer);
+
+$modalLocation = $(`<p class="modal-text cap"></p>`).appendTo(
+  $modalInfoContainer
+);
+
+$hr = $(`<hr>`).appendTo($modalInfoContainer);
+
+$modalPhone = $(`<p class="modal-text"></p>`).appendTo($modalInfoContainer);
+
+$modalAddress = $(`<p class="modal-text"></p>`).appendTo($modalInfoContainer);
+
+$modalBirthday = $(`<p class="modal-text"> Birthday: </p>`).appendTo(
+  $modalInfoContainer
+);
+
 $.ajax({
   url: "https://randomuser.me/api/?results=12",
   dataType: "json",
@@ -26,79 +58,78 @@ $.ajax({
           item.name.last
         } </h3>`
       ).appendTo($cardInfoContainer);
-      let $email = $(`<p class="card-text">${item.email}</p>`).appendTo(
+      let $cardEmail = $(`<p class="card-text">${item.email}</p>`).appendTo(
         $cardInfoContainer
       );
-      let $location = $(
+      let $cardLocation = $(
         `<p class="card-text cap">${item.location.city}</p>`
       ).appendTo($cardInfoContainer);
 
-      //BUILD MODAL
-      let $modal = $('<div class="modal"/>').appendTo($modalContainer);
-      let $btnClose = $(
-        '<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>'
-      ).appendTo($modal);
-      let $modalInfoContainer = $(
-        '<div class="modal-info-container">'
-      ).insertAfter($btnClose);
-      let $modalImage = $(
-        `<img class="modal-img" src="${
-          item.picture.medium
-        }" alt="profile picture">`
-      ).appendTo($modalInfoContainer);
-      let $modalName = $(
-        `<h3 id="name" class="modal-name cap">${item.name.first} ${
-          item.name.last
-        }
-  </h3>`
-      ).appendTo($modalInfoContainer);
-      let $modalEmail = $(`<p class="modal-text">${item.email}</p>`).appendTo(
-        $modalInfoContainer
-      );
-      let $modalLocation = $(
-        `<p class="card-text cap">${item.location.city}</p>`
-      ).appendTo($modalInfoContainer);
-      let $hr = $(`<hr>`).appendTo($modalInfoContainer);
-      let $modalPhone = $(`<p class="modal-text">${item.phone}</p>`).appendTo(
-        $modalInfoContainer
-      );
-      let $modalAddress = $(
-        `<p class="modal-text">${item.location.street}, ${
-          item.location.city
-        }, ${item.location.state} ${item.location.postcode}</p>`
-      ).appendTo($modalInfoContainer);
+      let $medImageURL = item.picture.medium;
+      let $firstName = item.name.first;
+      let $lastName = item.name.last;
+      let $email = item.email;
+      let $location = item.location.city;
+      let $phone = item.phone;
+      let $street = item.location.street;
+      let $city = item.location.city;
+      let $state = item.location.state;
+      let $postal = item.location.postcode;
 
       //Parse DOB
       let $dobToParse = item.dob.date;
       let $dobYearParsed = $dobToParse.substring(0, 4);
       let $dobMonthParsed = $dobToParse.substring(5, 7);
       let $dobDayParsed = $dobToParse.substring(8, 10);
-      console.log($dobDayParsed);
-
-      let $modalBirthday = $(
-        `<p class="modal-text"> Birthday: ${$dobMonthParsed}/${$dobDayParsed}/${$dobYearParsed}</p>`
-      ).appendTo($modalInfoContainer);
-
-      //BTN CLOSE EVENT
-      $btnClose.on("click", function() {
-        $.each($(".modal"), function(i, item) {
-          item.style.display = "none";
-          $modalContainer.css("display", "none");
-        });
-      });
 
       //CLICK CARD
       $card.on("click", function() {
-        console.log(event.target);
-
+        console.log($(this));
         if (
           event.target.parentNode.parentNode === $(".card")[i] ||
           event.target.parentNode === $(".card")[i] ||
           event.target === $(".card")[i]
         ) {
+          //Show modal:
           $modalContainer.css("display", "block");
-          document.getElementsByClassName("modal")[i].style.display = "block";
+          $modal.css("display", "block");
+
+          //Build modal with data
+          $(".modal-img").attr("src", $medImageURL);
+          $(".modal-name").text($firstName + " " + $lastName);
+          $(".modal-text:eq(0)").text($email);
+          $(".modal-text:eq(1)").text($city);
+          $(".modal-text:eq(2)").text($phone);
+          $(".modal-text:eq(3)").text(
+            $street + ", " + $city + ", " + $state + " " + $postal
+          );
+          $(".modal-text:eq(4)").text(
+            "Birthday: " +
+              $dobMonthParsed +
+              "/" +
+              $dobDayParsed +
+              "/" +
+              $dobYearParsed
+          );
+
+          /*$modalPhone = $(`<p class="modal-text">${$phone}</p>`).appendTo(
+            $modalInfoContainer
+          );
+          $modalAddress = $(
+            `<p class="modal-text">${$street}, ${$city}, ${$state} ${$postal}</p>`
+          ).appendTo($modalInfoContainer);
+          $modalBirthday = $(
+            `<p class="modal-text"> Birthday: ${$dobMonthParsed}/${$dobDayParsed}/${$dobYearParsed} </p>`
+          ).appendTo($modalInfoContainer);
+          console.log($medImageURL);*/
         }
+        //BTN CLOSE EVENT
+        $btnClose.on("click", function() {
+          $.each($(".modal"), function(i, item) {
+            item.style.display = "none";
+            $modalContainer.css("display", "none");
+          });
+        });
       });
     });
   }
