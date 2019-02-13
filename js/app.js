e327124f8eb7314224d142a2259d1502ca016193
Stacy <stacyrays@gmail.com>
2019-02-12 16:43:34 -0800

    Added search box does not function yet

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 //cd Desktop/LZ/Tutorials/Treehouse/TechDegree/techdegree-project-5
 //python -m SimpleHTTPServer
+const form = $(".search-container").append(`<form action="#" method="get">
+<input type="search" id="search-input" class="search-input" placeholder="Search...">
+<input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">
+</form>`);
+
 let $gallery = $("div#gallery.gallery");
 let $modalContainer = $('<div class="modal-container"/>').insertAfter($gallery);
 
