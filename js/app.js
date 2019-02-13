4a715f257b940936a0013ebdfcfe787fc1d7bef2
Stacy <stacyrays@gmail.com>
2019-02-13 12:24:45 -0800

    Fixed the search ID

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 //python -m SimpleHTTPServer
 const form = $(".search-container").append(`<form action="#" method="get">
 <input type="search" id="search-input" class="search-input" placeholder="Search...">
-<input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">
+<input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
 </form>`);
 
 const $gallery = $("#gallery");
